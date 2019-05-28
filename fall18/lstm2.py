154db6d3356846fcf95469d1c0fea716a401518e
Neil <neilwchen@gmail.com>
2019-05-27 20:58:58 -0400

    checkpoint

--- fall18/lstm2.py
+++ fall18/lstm2.py
@@ -1,14 +1,17 @@
 import os
 import sys 
+import argparse
 import numpy as np # linear algebra
-from scipy.stats import randint
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv), data manipulation as in SQL
+import pickle
 import matplotlib
 import matplotlib.pyplot as plt # this is used for the plot the graph 
 from matplotlib import patches
+from model import Model
 import seaborn as sns # used for plot interactive graph. 
 from sklearn.model_selection import train_test_split # to split the data into two parts
 
+from scipy.stats import randint
 import sklearn
 if sklearn.__version__ >"0.2.1":
     from sklearn.model_selection import KFold
@@ -50,29 +53,73 @@ def get_extrema(df, order=2000, ra=False):
 def plot_raw(df, n_cycles, savefig=None, figsize=(8, 4)):
     # one figure / two charts
     fig, ax = plt.subplots(nrows=2, ncols=1, figsize=figsize,dpi=300,sharex=True)
-    ax.flat[0].set_title(u'Raw measurements, %d cycles' % n_cycles)
+    ax.flat[0].set_title('Raw measurements, %d cycles' % n_cycles)
     x = df.timestamp
     # top chart
     ax.flat[0].plot(x,df.pwm, c='r', alpha=1,lw=0.5, label=u'PWM')
     ax2 = ax.flat[0].twinx()
     ax2.plot(x,df.f, alpha=0.8,lw=0.5, label='Force');
-    ax2.set_ylabel(u'Force [N]')
+    ax2.set_ylabel('Force [N]')
     ax.flat[0].legend(loc='best'); ax2.legend(loc='best')
-    ax.flat[0].set_ylabel(u'PWM')
+    ax.flat[0].set_ylabel('PWM')
     # bottom chart
-    ax.flat[1].plot(x,df.t,lw=0.5, label=u'Muscle temperature (C)');
-    ax.flat[1].plot(x,df.t0,lw=0.5, label=u'Environment temperature (C)')
+    ax.flat[1].plot(x,df.t,lw=0.5, label='Muscle temperature (C)');
+    ax.flat[1].plot(x,df.t0,lw=0.5, label='Environment temperature (C)')
     ax.flat[1].legend(loc='best')
-    ax.flat[1].set_ylabel(u'Temperature [°C]')
-    ax.flat[1].set_xlabel(u'time [s]')
+    ax.flat[1].set_ylabel('Temperature (C)')
+    ax.flat[1].set_xlabel('time [s]')
     plt.gca().set_xlim(left=0)
     plt.tight_layout()
     if savefig is not None:
         plt.savefig(savefig,dpi=300)
     plt.show()
 
-DATASET_NAME = '3tubes'
-df = load_dataset('data_2019-04-10-00-48-44.msgpack', window=150)
-n_cycles, minima, maxima = get_extrema(df)
-plot_raw(df, n_cycles=n_cycles, savefig=('plots/raw measurement,%s.png' % DATASET_NAME))
+def train_val_test_split(df, ratio=0.8):
+    if 'cycle' in df.columns:
+        data = df.drop(columns=['cycle'], inplace=False)
+    else:
+        data = df.copy()
+    data = np.apply_along_axis(lambda row: np.reshape(row, (1,-1)), 1, data.drop(columns=['timestamp']).values)
+    train_cutoff, val_cutoff = int(ratio*ratio * data.shape[0]), int(ratio * data.shape[0])
+    train, val, test = data[:train_cutoff,], data[train_cutoff:val_cutoff,], data[val_cutoff:,]
+    train_X, train_y = train[:,:,1:], train[:,:,0].flatten()
+    val_X, val_y = val[:,:,1:], val[:,:,0].flatten()
+    test_X, test_y = test[:,:,1:], test[:,:,0].flatten()
+    print("TRAIN_X: ", train_X.shape, " TRAIN_Y: ", train_y.shape)
+    print("VAL_X: ", val_X.shape, " VAL_Y: ", val_y.shape)
+    print("TEST_X: ", test_X.shape, " TEST_Y: ", test_y.shape)
+    return train_X, train_y, val_X, val_y, test_X, test_y
 
+
+if __name__=='__main__':
+
+    # Accept command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cuda', type=bool, default=False)
+    parser.add_argument('--plot', type=bool, default=True)
+    parser.add_argument('--train', type=bool, default=False)
+    args = parser.parse_args()
+    DATASET_NAME = '3tubes'
+    df = load_dataset('data_2019-04-10-00-48-44.msgpack', window=150)
+    n_cycles, minima, maxima = get_extrema(df)
+
+    ############################## Prediction ##############################
+
+    if not args.cuda:
+        os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+        os.environ['CUDA_VISIBLE_DEVICES'] = ''
+
+    train_X, train_y, val_X, val_y, test_X, test_y = train_val_test_split(df)
+    model = Model(input_shape=(train_X.shape[1], train_X.shape[2]))
+
+    if args.train:
+        model.train(train_X, train_y, val_X, val_y, name=('%d' % n_cycles), overwrite=True)
+
+    if args.plot:
+        plot_raw(df, n_cycles=n_cycles, savefig=('plots/raw measurement,%s.png' % DATASET_NAME))
+        model.plot_model()
+        model.plot_history()
+
+    val_y_hat = model.predict(val_X, y=val_y, plot=args.plot)
+    test_y_hat = model.predict(test_X, y=test_y, plot=args.plot)
+    pickle.save(
--- fall18/model.py
+++ fall18/model.py
@@ -1,34 +1,86 @@
 ############################################ LSTM & time-series prediction
 
 import keras
-from keras.layers import LSTM
+import os
 from matplotlib import pyplot as plt
+import keras
+from keras.layers import Dense, Dropout, Activation, LSTM
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.models import Sequential, model_from_json
+from keras.utils import to_categorical
+from keras.optimizers import SGD 
+from keras.callbacks import EarlyStopping
+from keras.utils import np_utils
+import itertools
 
 class Model(object):
-    def __init__(self):
+
+    def __init__(self, input_shape=(0,0)):
+        assert len(input_shape)==2
+        self.history = None
         self.model = Sequential()
-        self.model = Sequential()
-        self.model.add(LSTM(64, input_shape=(train_X.shape[1], train_X.shape[2])))
+        self.model.add(LSTM(64, input_shape=input_shape))
         self.model.add(Dense(32))
-        self.model.add(Dropuot(0.5))
+        self.model.add(Dropout(0.5))
         self.model.add(Dense(1))
         self.model.compile(loss='mean_squared_error', optimizer='adam')
+
+    def _save_model(self, name='X61'):
+        model_json = self.model.to_json()
+        with open('%s.json' % name, 'w') as json_file:
+            json_file.write(model_json)
+        self.model.save_weights('%s.h5' % name)
+        self.model.save('%s.hdf5' % name)
+
+    def _load_model(self, name='X61'):
+        json_file = open('%s.json' % name, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights('%s.h5' % name)
+        return loaded_model
 
     """
     Set `overwrite=True` to overwrite all weights in `weight_file.hdf5`.
     """
-    def train(self, name, train_X, train_y, val_X, val_y, overwrite=False, epochs=20, batch_size=10000):
-        if not os.path.isfile('%s.hdf5' % name) or overwrite:
-            history = self.model.fit(train_X, train_y, epochs=epochs, batch_size=batch_size, validation_data=(val_X, val_y), verbose=2, shuffle=True)
-            save_model(self.model, name)
+    def train(self, train_X, train_y, val_X, val_y, name='X61', overwrite=False, epochs=20, batch_size=10000):
+        history = None
+        if overwrite or not os.path.isfile('%s.hdf5' % name):
+            self.history = self.model.fit(train_X, train_y, epochs=epochs, batch_size=batch_size, validation_data=(val_X, val_y), verbose=2, shuffle=True)
+            self._save_model(name)
         else:
-            model = load_model(name)
-            history = None
-        return model, history
+            self.model = self._load_model(name)
 
-    def plot_model(self, image_path = 'plots/model.png'):
+    def predict(self, X, y=None, plot=True):
+        yhat = self.model.predict(X)
+        if plot:
+            fig, ax = plt.subplots(figsize=(8, 4), dpi=300)
+            ax.plot(yhat, alpha=0.8, c='r', label='Predicted force')
+            if 'numpy' in str(type(y)):
+                ax.plot(y, alpha=0.3, c='b', label='Measured force')
+            ax.set_title('Prediction on test data')
+            ax.set_xlabel('Time step')
+            ax.set_ylabel('Force (N)')
+            plt.legend(loc='best')
+            plt.tight_layout()
+            plt.savefig('plots/prediction.png', dpi=300)
+            plt.show()
+        return yhat
+
+    def plot_model(self, image_path='plots/model.png'):
         keras.utils.plot_model(self.model, show_shapes=True, to_file=image_path)
         plt.figure(dpi=150)
         imgplot = plt.imshow(plt.imread(image_path))
         plt.axis('off')
         plt.show()
+
+    def plot_history(self, n_epochs=20, n_cycles=100):
+        if self.history:
+            plt.plot(self.history.history['loss'])
+            plt.plot(self.history.history['val_loss'])
+            plt.title('LSTM loss (MSE) on %d epochs, %d cycles' % (n_epochs, n_cycles))
+            plt.ylabel('loss')
+            plt.xlabel('epoch')
+            plt.legend(['train', 'validation'], loc='upper right')
+            plt.show()
